c3f7fc8fb53d7f7d2d51b7aceaa95d7cc85d38fb
samaj89 <samajansen89@gmail.com>
2018-06-26 10:19:20 +0100

    Removed Twitter case class, using Tuple4 instead; added sink to CSV file output

--- src/main/scala/twitter/TwitterStreamReader.scala
+++ src/main/scala/twitter/TwitterStreamReader.scala
@@ -6,21 +6,19 @@ import java.util.Properties
 import com.github.nscala_time.time.Imports.DateTimeFormat
 import net.liftweb.json.JsonAST.JNothing
 import net.liftweb.json.{JString, parse}
+import org.apache.flink.core.fs.FileSystem.WriteMode
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.connectors.twitter.TwitterSource
+import org.apache.flink.table.api.TableEnvironment
+import org.apache.flink.table.api.scala.StreamTableEnvironment
+import org.apache.flink.table.sinks.CsvTableSink
 
 object TwitterStreamReader {
 
   def main(args: Array[String]) {
 
-    case class Tweet(
-                            id:String,
-                            creationTime:Long,
-                            user:String,
-                            text:String
-                          )
-
     val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val tableEnv: StreamTableEnvironment = TableEnvironment.getTableEnvironment(env)
 
     val prop = new Properties()
     val propFilePath = "/home/samaj/twitter.properties"
@@ -49,24 +47,23 @@ object TwitterStreamReader {
       }
     )
 
-    val structuredStream:DataStream[Tweet] = parsedStream.filter(
+    val structuredStream:DataStream[Tuple4[String, Long, String, String]] = parsedStream.filter(
       record => record \ "retweeted_status" == JNothing && (record \ "lang" \\ classOf[JString] ).head.toString == "en").map(
       record => {
-        Tweet(
-          ( record \ "id_str" \\ classOf[JString] ).head.toString
+        (( record \ "id_str" \\ classOf[JString] ).head.toString
           , DateTimeFormat
             .forPattern("EEE MMM dd HH:mm:ss Z yyyy")
             .parseDateTime(
               ( record \ "created_at" \\ classOf[JString] ).head
             ).getMillis
           , ( record \ "user" \ "name" \\ classOf[JString] ).head.toString
-          , ( record \ "text" \\ classOf[JString] ).head.toString
-        )
-
+          , ( record \ "text" \\ classOf[JString] ).head.toString)
       }
     )
 
-    structuredStream.print
+    val tweetTable = tableEnv.fromDataStream(structuredStream)
+
+    tweetTable.writeToSink(new CsvTableSink("/home/samaj/test", fieldDelim = "|", numFiles = 1, writeMode = WriteMode.OVERWRITE))
 
     env.execute("Twitter Stream Reader")
   }
