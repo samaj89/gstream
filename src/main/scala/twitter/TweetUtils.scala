4a85452dcf48348d238fc7bca2a5309d308738b0
samaj89 <samajansen89@gmail.com>
2018-07-17 11:53:53 +0100

    Added stopwords file

--- src/main/scala/twitter/TweetUtils.scala
+++ src/main/scala/twitter/TweetUtils.scala
@@ -1,10 +1,15 @@
 package twitter
 
+import scala.io.Source
+
 /** An object providing several methods used to pre-process tweets from Twitter stream
   * before they are used as input for the gstream algorithm.
   */
 
 object TweetUtils {
+
+  val file = "/home/samaj/ranksnl_large.txt"
+  final val stopwords = Source.fromFile(file).getLines.toList
 
   def splitTweet(s: String): List[String] = if (s.isEmpty) List() else s.trim.split("\\s+").toList
 
